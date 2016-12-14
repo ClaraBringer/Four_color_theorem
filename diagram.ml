4d3fc5e9f095ea3de2e0d59ebdae25409de46e7b
ClaraBringer <clara.bringer@hotmail.fr>
2016-12-14 23:42:06 +0100

    Résolution de diagramme, colorier, modifier ou supprimer une région, gagner, commentaires

--- diagram.ml
+++ diagram.ml
@@ -1,7 +1,7 @@
 open Graphics;;
 
 type seed = {
-  c : color option;
+  mutable c : color option;
   x : int;
   y : int
 }
@@ -121,24 +121,37 @@ let v4 = {
   |]
 }
 
+(* Construit une chaine de caractères à partir d'une dimension
+   (i, j) : dimension
+   return : chaine de caractères *)
 let string_of_dim (i, j) =
   (string_of_int i)^("x")^(string_of_int j)
 ;;
 
+(* Crée une distance
+   q : entier
+   return : une distance *)
 let make_distance q =
   fun (x1, y1) (x2, y2) -> (float_of_int (abs (x1 - x2)))**q +. (float_of_int (abs (y1 - y2)))**q
 ;;
 
+(* Crée la distance euclidienne
+   return : la distance euclidienne *)
 let euclidean_distance =
   make_distance 2.
 ;;
 
+(* Crée la distance taxicab
+   return : la distance taxicab *)
 let taxicab_distance =
   make_distance 1.
 ;;
 
-(* Construit la matrice des régions du diagramme *)
-let regions_voronoi distance_function voronoi =
+(* Construit la matrice des régions du diagramme
+   distance_function : une fonction de distance
+   voronoi : un diagramme de Voronoi
+   return : une matrice d'entiers *)
+let regions_voronoi voronoi distance_function =
   let lines = snd voronoi.dim and
   columns = fst voronoi.dim and
   seeds_number = Array.length voronoi.seeds in
@@ -161,24 +174,25 @@ let regions_voronoi distance_function voronoi =
   matrix
 ;;
 
-(* Dessine le diagramme *)
-let draw_voronoi distance_function voronoi =
-  let matrix = regions_voronoi distance_function voronoi in
-  let columns = Array.length matrix and
-  lines = Array.length matrix.(0) in
+(* Dessine le diagramme
+   voronoi : un diagramme de Voronoi
+   matrix_regions : la matrice des régions associée à voronoi *)
+let draw_voronoi voronoi matrix_regions =
+  let columns = Array.length matrix_regions and
+  lines = Array.length matrix_regions.(0) in
   open_graph (" "^(string_of_int columns)^"x"^(string_of_int lines));
   auto_synchronize false;
   for x = 0 to (columns - 1) do
     for y = 0 to (lines - 1) do
-      if (((x > 1) && (matrix.(x).(y) <> matrix.(x-1).(y)))
-          || ((x < columns - 1) && (matrix.(x).(y) <> matrix.(x+1).(y)))
-          || ((y > 1) && (matrix.(x).(y) <> matrix.(x).(y-1)))
-          || ((y < lines - 1) && (matrix.(x).(y) <> matrix.(x).(y+1))))
+      if (((x > 1) && (matrix_regions.(x).(y) <> matrix_regions.(x-1).(y)))
+          || ((x < columns - 1) && (matrix_regions.(x).(y) <> matrix_regions.(x+1).(y)))
+          || ((y > 1) && (matrix_regions.(x).(y) <> matrix_regions.(x).(y-1)))
+          || ((y < lines - 1) && (matrix_regions.(x).(y) <> matrix_regions.(x).(y+1))))
       then
         set_color black
       else
         begin
-          match voronoi.seeds.(matrix.(x).(y)).c with
+          match voronoi.seeds.(matrix_regions.(x).(y)).c with
           | None -> set_color white
           | Some(rgb) -> set_color rgb
         end;
@@ -188,11 +202,14 @@ let draw_voronoi distance_function voronoi =
   synchronize ()
 ;;
 
-(* Construit la matrice d'adjacence des régions du diagramme *)
-let adjacences_voronoi voronoi matrix =
+(* Construit la matrice d'adjacence des régions du diagramme
+   voronoi : un diagramme de Voronoi
+   matrix_regions : la matrice des régions associée à voronoi
+   return : une matrice de booléens *)
+let adjacences_voronoi voronoi matrix_regions =
   let seeds_number = Array.length voronoi.seeds and
-  columns = Array.length matrix and
-  lines = Array.length matrix.(0) in
+  columns = Array.length matrix_regions and
+  lines = Array.length matrix_regions.(0) in
   let adjacences = Array.make_matrix seeds_number seeds_number false in
   for h = 0 to (seeds_number - 1)  do
     for k = 0 to (seeds_number - 1) do
@@ -200,10 +217,10 @@ let adjacences_voronoi voronoi matrix =
         begin
           for i = 0 to (columns - 1) do
             for j = 0 to (lines - 1) do
-              if (matrix.(i).(j) = h && (((i > 0) && matrix.(i-1).(j) = k)
-                                         || ((i < columns - 1) && matrix.(i+1).(j) = k)
-                                         || ((j > 0) && matrix.(i).(j-1) = k)
-                                         || ((j < lines - 1) && matrix.(i).(j+1) = k)))
+              if (matrix_regions.(i).(j) = h && (((i > 0) && matrix_regions.(i-1).(j) = k)
+                                         || ((i < columns - 1) && matrix_regions.(i+1).(j) = k)
+                                         || ((j > 0) && matrix_regions.(i).(j-1) = k)
+                                         || ((j < lines - 1) && matrix_regions.(i).(j+1) = k)))
               then
                 adjacences.(h).(k) <- true
             done;
@@ -214,7 +231,9 @@ let adjacences_voronoi voronoi matrix =
   adjacences
 ;;
 
-(* Construit le tableau des couleurs des régions *)
+(* Construit le tableau des couleurs des régions
+   voronoi : un diagramme de Voronoi
+   return : un tableau de color option *)
 let is_seed_colored voronoi =
   let seeds_number = Array.length voronoi.seeds in
   let colored_seeds = Array.make seeds_number None in
@@ -226,7 +245,9 @@ let is_seed_colored voronoi =
   colored_seeds
 ;;
 
-(* Vérifie s'il existe une couleur à une région *)
+(* Vérifie s'il existe une couleur à une région
+   colored_seeds : un tableau de couleur des régions
+   return : une liste de clauses *)
 let exists colored_seeds =
   let colors = [|red; green; blue; yellow|] in
   let seeds_number = Array.length colored_seeds and
@@ -241,7 +262,9 @@ let exists colored_seeds =
   !exist_array
 ;;
 
-(* Vérifie que chaque région a au plus une couleur *)
+(* Vérifie que chaque région a au plus une couleur
+   colored_seeds : un tableau de couleur des régions
+   return : une liste de clauses *)
 let unique colored_seeds =
   let colors = [|red; green; blue; yellow|] in
   let seeds_number = Array.length colored_seeds and
@@ -257,32 +280,108 @@ let unique colored_seeds =
   !unique_array
 ;;
 
-(* Vérifie que deux régions adjacentes n'ont pas la même couleur *)
-let adjacent colored_seeds adjacences =
+(* Vérifie que deux régions adjacentes n'ont pas la même couleur
+   colored_seeds : un tableau de couleur des régions
+   adjacences_matrix : un tableau d'adjacence des régions
+   return : une liste de clauses *)
+let adjacent colored_seeds adjacences_matrix =
   let colors = [|red; green; blue; yellow|] in
   let seeds_number = Array.length colored_seeds and
-  adjacent_matrix = ref [] in
+  adjacent = ref [] in
   for i = 0 to (seeds_number - 1) do
     for i' = 0 to (seeds_number - 1) do
-      if adjacences.(i).(i') then
+      if adjacences_matrix.(i).(i') then
         for c = 0 to 3 do
-          adjacent_matrix := [(false, (i, colors.(c))); (false, (i', colors.(c)))]::(!adjacent_matrix)
+          adjacent := [(false, (i, colors.(c))); (false, (i', colors.(c)))]::(!adjacent)
         done;
     done;
   done;
-  !adjacent_matrix
+  !adjacent
 ;;
 
+(* Produit la FNC (liste de contraintes)
+   colored_seeds : un tableau de couleur des régions
+   adjacences_matrix : un tableau d'adjacence des régions
+   return : une liste de clauses *)
 let produce_constraints colored_seeds adjacences_matrix =
   (exists colored_seeds)@(unique colored_seeds)@(adjacent colored_seeds adjacences_matrix)
 ;;
 
-let colored_seeds = is_seed_colored v1;;
-let matrix = regions_voronoi euclidean_distance v1;;
-let adjacences_matrix = adjacences_voronoi v1 matrix;;
+(* Renvoie une solution d'un diagramme
+   voronoi : un diagramme de Voronoi
+   constraints : la liste des contraintes
+   return : un diagramme de Voronoi *)
+let solve_voronoi voronoi constraints =
+  let solution = Solver.solve constraints in
+  match solution with
+  | None -> failwith "Aucune solution"
+  | Some(l) -> begin
+      for i = 0 to ((List.length l) - 1) do
+        let index = fst (snd (List.nth l i)) and
+          color = snd (snd (List.nth l i)) in
+        voronoi.seeds.(index).c <- Some(color)
+      done;
+    end;
+  voronoi
+;;
 
-let s = Solver.solve (produce_constraints colored_seeds adjacences_matrix);;
+(* Colorie une région non encore coloriée
+   voronoi : un diagramme de Voronoi
+   index : l'index dans le tableau de germes de la région
+   color : une couleur *)
+let color_region voronoi index color =
+  if voronoi.seeds.(index).c = None then
+    voronoi.seeds.(index).c <- Some(color)
+;;
 
-(* draw_voronoi (make_distance 2.) v1;;
+(* Modifie une région coloriée : si color vaut white, la couleur est supprimée, sinon la couleur est modifiée
+   voronoi : un diagramme de Voronoi
+   initial_colored_seeds : le tableau initial des couleurs des régions
+   index : l'index dans le tableau de germes de la région
+   color : une couleur *)
+let change_region voronoi initial_colored_seeds index color =
+  match initial_colored_seeds.(index).c with
+  | None -> begin
+      if color = white then
+        voronoi.seeds.(index).c <- None
+      else
+        voronoi.seeds.(index).c <- Some(color)
+    end
+  | Some(rgb) -> print_string "Vous ne pouvez pas modifier cette région !"
+;;
 
-wait_next_event [Button_down];; *)
+(* Vérifie si la configuration est gagnante
+   voronoi : un diagramme de Voronoi
+   adjacences_matrix : un tableau d'adjacence des régions
+   return : false s'il y a au moins une erreur, true sinon *)
+let win voronoi adjacences_matrix =
+  let win = ref true in
+  let seeds_number = Array.length voronoi.seeds in
+  for i = 0 to (seeds_number - 1) do
+    match voronoi.seeds.(i).c with
+    | None -> win := false
+    | Some(rgb) -> begin
+        for j = 0 to (seeds_number - 1) do
+          if adjacences_matrix.(i).(j) = true then
+            if voronoi.seeds.(j).c = Some(rgb) then
+              win := false
+        done;
+      end;
+  done;
+  !win
+;;
+
+(* Fonction main *)
+let main diagram =
+  (* TODO : choisir aléatoirement un diagramme au lieu de le passer en argument *)
+  let matrix_regions = regions_voronoi diagram euclidean_distance in
+  let initial_colored_seeds = is_seed_colored diagram and
+  adjacences_matrix = adjacences_voronoi diagram matrix_regions in
+  let constraints = produce_constraints initial_colored_seeds adjacences_matrix in
+  let solved_diagram = solve_voronoi diagram constraints in
+  draw_voronoi solved_diagram matrix_regions;
+
+  wait_next_event [Button_down]
+;;
+
+main v4;;
